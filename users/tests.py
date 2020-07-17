0dc995c144ae931a0d86cf7dab9c7d4e6463224d
Danilo Reitano <daniloreitanoandrades@gmail.com>
2020-07-16 23:21:38 -0300

    Finished User Testing and fixes

--- users/tests.py
+++ users/tests.py
@@ -3,6 +3,7 @@ from rest_framework.test import APIClient
 from rest_framework.authtoken.models import Token
 from users.models import User, Group
 from django.contrib.auth.models import Permission
+from instituciones.models import Institucion
 
 
 class AuthenticationTests(APITestCase):
@@ -96,29 +97,51 @@ class PermissionsTests(APITestCase):
 
 
 class UsersTests(APITestCase):
-    def setUp(self):
-        self.client = APIClient()
-        self.group_admin = Group.objects.create(name="Admin1")
-        self.group_admin.save()
-        self.group_admin.permissions.add(Permission.objects.get(name="Can view group"))
-        self.group_admin.permissions.add(Permission.objects.get(name="Can view institucion"))
-        self.group_admin.permissions.add(Permission.objects.get(name="Can add user"))
-        self.group_admin.permissions.add(Permission.objects.get(name="Can edit other users info"))
-        self.group_admin.permissions.add(Permission.objects.get(name="Can change user"))
-        self.group_admin.permissions.add(Permission.objects.get(name="Can change status of User"))
-        self.group_admin.permissions.add(Permission.objects.get(name="Can view user"))
+    @classmethod
+    def setUpTestData(cls):
+        cls.client = APIClient()
+        cls.group_admin = Group.objects.create(name="Admin1")
+        cls.group_admin.save()
+        cls.group_admin.permissions.add(Permission.objects.get(name="Can view group"))
+        cls.group_admin.permissions.add(Permission.objects.get(name="Can view institucion"))
+        cls.group_admin.permissions.add(Permission.objects.get(name="Can add user"))
+        cls.group_admin.permissions.add(Permission.objects.get(name="Can edit other users info"))
+        cls.group_admin.permissions.add(Permission.objects.get(name="Can change user"))
+        cls.group_admin.permissions.add(Permission.objects.get(name="Can change status of User"))
+        cls.group_admin.permissions.add(Permission.objects.get(name="Can view user"))
 
-        self.group_docente = Group.objects.create(name="Docente1")
-        self.group_docente.save()
-        self.group_docente.permissions.add(Permission.objects.get(name="Can view group"))
-        self.group_docente.permissions.add(Permission.objects.get(name="Can view institucion"))
-        self.group_docente.permissions.add(Permission.objects.get(name="Can change user"))
-        self.group_docente.permissions.add(Permission.objects.get(name="Can view user"))
-        self.user_admin = User.objects.create_user("juan@juan.com", password="password", groups=self.group_admin)
-        self.user_docente = User.objects.create_user("juan2@juan.com", password="password", groups=self.group_docente)
+        cls.group_docente = Group.objects.create(name="Docente1")
+        cls.group_docente.save()
+        cls.group_docente.permissions.add(Permission.objects.get(name="Can view group"))
+        cls.group_docente.permissions.add(Permission.objects.get(name="Can view institucion"))
+        cls.group_docente.permissions.add(Permission.objects.get(name="Can change user"))
+        cls.group_docente.permissions.add(Permission.objects.get(name="Can view user"))
+
+        cls.institucion_1 = Institucion.objects.create(nombre="Institucion_1")
+        cls.institucion_2 = Institucion.objects.create(nombre="Institucion_2")
+
+        cls.user_admin_1 = User.objects.create_user(
+            "juan1@juan.com", password="password", groups=cls.group_admin, institucion=cls.institucion_1
+        )
+        cls.user_docente_1 = User.objects.create_user(
+            "juan2@juan.com", password="password", groups=cls.group_docente, institucion=cls.institucion_1
+        )
+        cls.user_docente_3 = User.objects.create_user(
+            "juan3@juan.com", password="password", groups=cls.group_docente, institucion=cls.institucion_1
+        )
+
+        cls.user_admin_2 = User.objects.create_user(
+            "juan4@juan.com", password="password", groups=cls.group_admin, institucion=cls.institucion_2
+        )
+        cls.user_docente_2 = User.objects.create_user(
+            "juan5@juan.com", password="password", groups=cls.group_docente, institucion=cls.institucion_2
+        )
+        cls.user_docente_4 = User.objects.create_user(
+            "juan6@juan.com", password="password", groups=cls.group_docente, institucion=cls.institucion_2
+        )
 
     def test_create_user_admin_authenticated(self):
-        token = Token.objects.create(user=self.user_admin)
+        token = Token.objects.create(user=self.user_admin_1)
         new_user = {
             "email": "pedro@pedro.com",
             "password": "pedrito123",
@@ -131,7 +154,7 @@ class UsersTests(APITestCase):
         self.assertEqual(response.status_code, 201)
 
     def test_create_user_admin_not_authenticated(self):
-        token = Token.objects.create(user=self.user_admin)
+        token = Token.objects.create(user=self.user_admin_1)
         new_user = {
             "email": "pedro@pedro.com",
             "password": "pedrito123",
@@ -142,7 +165,7 @@ class UsersTests(APITestCase):
         self.assertEqual(response.status_code, 401)
 
     def test_create_user_admin_not_email(self):
-        token = Token.objects.create(user=self.user_admin)
+        token = Token.objects.create(user=self.user_admin_1)
         new_user = {
             "password": "pedrito123",
             "password2": "pedrito123",
@@ -154,7 +177,7 @@ class UsersTests(APITestCase):
         self.assertEqual(response.status_code, 400)
 
     def test_create_user_admin_not_matching_passwords(self):
-        token = Token.objects.create(user=self.user_admin)
+        token = Token.objects.create(user=self.user_admin_1)
         new_user = {
             "email": "pedro@pedro.com",
             "password": "pedrito13",
@@ -167,7 +190,7 @@ class UsersTests(APITestCase):
         self.assertEqual(response.status_code, 400)
 
     def test_create_user_docente(self):
-        token = Token.objects.create(user=self.user_docente)
+        token = Token.objects.create(user=self.user_docente_1)
         new_user = {
             "email": "pedro@pedro.com",
             "password": "pedrito13",
@@ -176,5 +199,149 @@ class UsersTests(APITestCase):
         }
         response = self.client.post(
             "/api/users/", data=new_user, format="json", HTTP_AUTHORIZATION="Token " + token.key,
+        )
+        self.assertEqual(response.status_code, 403)
+
+    def test_list_(self):
+        token = Token.objects.create(user=self.user_docente_1)
+        response = self.client.get("/api/users/list/", HTTP_AUTHORIZATION="Token " + token.key,)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 2)
+
+    def test_destroy_user_docente(self):
+        token = Token.objects.create(user=self.user_docente_1)
+        response = self.client.delete("/api/users/1/", HTTP_AUTHORIZATION="Token " + token.key,)
+        self.assertEqual(response.status_code, 403)
+
+    def test_destroy_user_admin(self):
+        token = Token.objects.create(user=self.user_admin_1)
+        response = self.client.delete("/api/users/1/", HTTP_AUTHORIZATION="Token " + token.key,)
+        self.assertEqual(response.status_code, 403)
+
+    def test_destroy_user_error(self):
+        token = Token.objects.create(user=self.user_admin_1)
+        response = self.client.delete("/api/users/x/", HTTP_AUTHORIZATION="Token " + token.key,)
+        self.assertEqual(response.status_code, 404)
+
+    def test_get_user_admin_failed(self):
+        token = Token.objects.create(user=self.user_admin_1)
+        response = self.client.get("/api/users/14/", HTTP_AUTHORIZATION="Token " + token.key,)
+        self.assertEqual(response.status_code, 404)
+
+    def test_get_user_admin_correct(self):
+        token = Token.objects.create(user=self.user_admin_1)
+        response = self.client.get("/api/users/13/", HTTP_AUTHORIZATION="Token " + token.key,)
+        self.assertDictContainsSubset({"id": 13}, response.data)
+        self.assertEqual(response.status_code, 200)
+
+    def test_status_user_admin_correct(self):
+        token = Token.objects.create(user=self.user_admin_1)
+        estado = {"is_active": "false"}
+        response = self.client.patch(
+            "/api/users/13/status/", data=estado, format="json", HTTP_AUTHORIZATION="Token " + token.key,
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_status_user_admin_incorrect(self):
+        token = Token.objects.create(user=self.user_admin_1)
+        estado = {"is_active": "false"}
+        response = self.client.patch(
+            "/api/users/11/status/", data=estado, format="json", HTTP_AUTHORIZATION="Token " + token.key,
+        )
+        self.assertEqual(response.status_code, 400)
+
+    def test_status_user_admin_redundant(self):
+        token = Token.objects.create(user=self.user_admin_1)
+        estado = {"is_active": "true"}
+        response = self.client.patch(
+            "/api/users/12/status/", data=estado, format="json", HTTP_AUTHORIZATION="Token " + token.key,
+        )
+        self.assertEqual(response.status_code, 202)
+
+    def test_status_user_admin_bad_request(self):
+        token = Token.objects.create(user=self.user_admin_1)
+        estado = {"is_active": ""}
+        response = self.client.patch(
+            "/api/users/13/status/", data=estado, format="json", HTTP_AUTHORIZATION="Token " + token.key,
+        )
+        self.assertEqual(response.status_code, 400)
+
+    def test_status_user_docente(self):
+        token = Token.objects.create(user=self.user_docente_1)
+        estado = {"is_active": "false"}
+        response = self.client.patch(
+            "/api/users/13/status/", data=estado, format="json", HTTP_AUTHORIZATION="Token " + token.key,
+        )
+        self.assertEqual(response.status_code, 403)
+
+    def test_status_user_admin_other_institution(self):
+        token = Token.objects.create(user=self.user_admin_1)
+        estado = {"is_active": "true"}
+        response = self.client.patch(
+            "/api/users/15/status/", data=estado, format="json", HTTP_AUTHORIZATION="Token " + token.key,
+        )
+        self.assertEqual(response.status_code, 404)
+
+    def test_update_user_admin_not_existing_user(self):
+        token = Token.objects.create(user=self.user_admin_1)
+        data = {"email": "hola@hola.com"}
+        response = self.client.patch(
+            "/api/users/25/", data=data, format="json", HTTP_AUTHORIZATION="Token " + token.key,
+        )
+        self.assertEqual(response.status_code, 404)
+
+    def test_update_user_admin_self(self):
+        token = Token.objects.create(user=self.user_admin_1)
+        data = {"email": "hola@hola.com"}
+        response = self.client.patch(
+            "/api/users/11/", data=data, format="json", HTTP_AUTHORIZATION="Token " + token.key,
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_update_user_admin_other(self):
+        token = Token.objects.create(user=self.user_admin_1)
+        data = {"email": "hola2@hola.com"}
+        response = self.client.patch(
+            "/api/users/12/", data=data, format="json", HTTP_AUTHORIZATION="Token " + token.key,
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_update_user_admin_other_institucion(self):
+        token = Token.objects.create(user=self.user_admin_1)
+        data = {"email": "hola3@hola.com"}
+        response = self.client.patch(
+            "/api/users/15/", data=data, format="json", HTTP_AUTHORIZATION="Token " + token.key,
+        )
+        self.assertEqual(response.status_code, 400)
+
+    def test_update_user_admin_other_not_active(self):
+        token = Token.objects.create(user=self.user_admin_1)
+        data = {"email": "hola4@hola.com"}
+        response = self.client.patch(
+            "/api/users/13/", data=data, format="json", HTTP_AUTHORIZATION="Token " + token.key,
+        )
+        self.assertEqual(response.status_code, 400)
+
+    def test_update_user_admin_other_not_active(self):
+        token = Token.objects.create(user=self.user_admin_1)
+        data = {"email": "juan4@juan.com"}
+        response = self.client.patch(
+            "/api/users/12/", data=data, format="json", HTTP_AUTHORIZATION="Token " + token.key,
+        )
+        self.assertEqual(response.status_code, 400)
+
+    def test_update_user_docente_self(self):
+        token = Token.objects.create(user=self.user_docente_1)
+        data = {"email": "hola7@hola.com"}
+        response = self.client.patch(
+            "/api/users/12/", data=data, format="json", HTTP_AUTHORIZATION="Token " + token.key,
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_update_user_docente_self(self):
+        token = Token.objects.create(user=self.user_docente_1)
+        data = {"email": "hola8@hola.com"}
+        response = self.client.patch(
+            "/api/users/13/", data=data, format="json", HTTP_AUTHORIZATION="Token " + token.key,
         )
         self.assertEqual(response.status_code, 403)
