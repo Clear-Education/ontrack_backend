750e3ac5aaa8dabeaa42160f198076acc06a844b
Wences <villegaswences@gmail.com>
2020-09-06 16:10:25 -0300

    Test fix #2

--- seguimientos/tests.py
+++ seguimientos/tests.py
@@ -31,9 +31,7 @@ class SeguimientosTest(APITestCase):
         cls.group.permissions.add(
             *Permission.objects.values_list("id", flat=True)
         )
-        cls.institucion = Institucion.objects.create(
-            nombre="MIT", cuit=1101010
-        )
+        cls.institucion = Institucion.objects.create(nombre="MIT")
         cls.user = User.objects.create_user(
             "juan@juan.com",
             password="juan123",
