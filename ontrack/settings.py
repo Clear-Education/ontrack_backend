c7023e48d24c41881ccf7cb7a70fd3c0a2fb1111
Wences <villegaswences@gmail.com>
2020-09-15 14:03:30 -0300

    Configuration ready!

--- ontrack/settings.py
+++ ontrack/settings.py
@@ -65,6 +65,7 @@ INSTALLED_APPS = [
     "asistencias",
     "seguimientos",
     "objetivos",
+    "storages",
 ]
 
 REST_FRAMEWORK = {
@@ -178,16 +179,26 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "ontrack", "static"),
-]
+MEDIA_URL = "/images/"
 
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
+MEDIA_ROOT = os.path.join(BASE_DIR, "static/images")
+
+
+# S3 BUCKETS CONFIG
+
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_S3_ADDRESSING_STYLE = "virtual"
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+AWS_S3_SIGNATURE_VERSION = "s3v4"
+AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME")
 
 AUTH_USER_MODEL = "users.User"
 
