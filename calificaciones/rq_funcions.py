b5cd681653b3b0bf1cc9184286895d5508661ff5
Danilo Reitano <daniloreitanoandrades@gmail.com>
2020-10-23 00:54:25 -0300

    test wip

--- calificaciones/rq_funcions.py
+++ calificaciones/rq_funcions.py
@@ -58,6 +58,7 @@ def calculate_promedio_for_objetivo(
     objetivo, alumno_curso, date_recalculate, calificaciones
 ):
     materias = set([m.id for m in objetivo.seguimiento.materias.all()])
+
     fechas = sorted(
         list(
             {
